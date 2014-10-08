0d00c10cea81b40158198933768fd29dd17714bd
Brian Clozel <bclozel@pivotal.io>
2014-10-08 14:19:30 +0200

    Use Aggressive options for the JVM

--- bench.sh
+++ bench.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mvn clean package
-java -jar target/benchmarks.jar -f 1 -wi 5 -i 5 -r 3s -jvmArgs '-server' .*Benchmark.*
-# java -jar target/benchmarks.jar -f 1 -wi 5 -i 5 -r 3s -jvmArgs '-server -XX:+AggressiveOpts' .*Benchmark.*
+java -jar target/benchmarks.jar -f 1 -wi 5 -i 5 -r 3s -jvmArgs '-server -XX:+AggressiveOpts' .*Benchmark.*
